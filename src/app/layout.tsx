89c8d7d1fa120ecf4d96ae44cd8c097babe5c0f9
karasique22 <timofeidudin@mail.ru>
2026-04-19 19:54:00 +0300

    feat: add seo metadata and json-ld

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,33 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin', 'cyrillic'] })
 
 export const metadata: Metadata = {
-	title: 'UST Landing',
-	description: 'UST Landing page'
+	title: 'Устойчивые продукты онлайн-образования | Магистратура Университета Косыгина',
+	description:
+		'Магистерская программа нового поколения. Диплом как стартап, профессиональный трекинг, 2 квалификации в одном дипломе. Поступление по портфолио.',
+	keywords: [
+		'магистратура',
+		'онлайн-образование',
+		'EdTech',
+		'диплом как стартап',
+		'профессиональный трекинг',
+		'Университет Косыгина',
+	],
+	openGraph: {
+		title: 'Устойчивые продукты онлайн-образования',
+		description: 'Магистерская программа нового поколения Университета Косыгина',
+		type: 'website',
+		locale: 'ru_RU',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Устойчивые продукты онлайн-образования',
+		description: 'Магистерская программа нового поколения Университета Косыгина',
+	},
+	robots: { index: true, follow: true },
 }
 
 export default function RootLayout({
-	children
+	children,
 }: {
 	children: React.ReactNode
 }) {
@@ -23,6 +44,35 @@ export default function RootLayout({
 				<Toaster
 					position="bottom-center"
 					richColors
+				/>
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{
+						__html: JSON.stringify({
+							'@context': 'https://schema.org',
+							'@type': 'EducationalOrganization',
+							name: 'Университет Косыгина',
+							url: 'https://rguk.ru',
+							description:
+								'Магистерская программа «Устойчивые продукты онлайн-образования»',
+							hasOfferCatalog: {
+								'@type': 'OfferCatalog',
+								name: 'Магистерские программы',
+								itemListElement: [
+									{
+										'@type': 'Course',
+										name: 'Устойчивые продукты онлайн-образования',
+										description: 'Магистерская программа нового поколения',
+										teaches: [
+											'Разработка образовательных продуктов',
+											'Управление EdTech-платформами',
+											'ИИ в образовании',
+										],
+									},
+								],
+							},
+						}),
+					}}
 				/>
 			</body>
 		</html>
